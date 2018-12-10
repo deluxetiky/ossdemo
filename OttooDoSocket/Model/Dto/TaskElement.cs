d860e6c26e802d0ae9381e4749f47f6bac689515
Hamit Yılmaz <hamityilmaz@outlook.com>
2018-12-10 17:09:04 +0300

    Update task element model in socket

--- OttooDoSocket/Model/Dto/TaskElement.cs
+++ OttooDoSocket/Model/Dto/TaskElement.cs
@@ -8,5 +8,10 @@ namespace OttooDoSocket.Model.Dto
     public class TaskElementDto
     {
         public string Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Explanation { get; set; } = "";
+        public int FavoriteCount { get; set; } = 0;
+        public string CreatedBy { get; set; } = "";
+        public DateTime LastUpdatedTime { get; set; } = DateTime.UtcNow;
     }
 }
